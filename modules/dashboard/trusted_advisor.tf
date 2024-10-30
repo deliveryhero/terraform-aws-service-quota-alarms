46aab2211a3b424a6065e54ee473abee1598c1a7
Max Williams <max-rocket-internet@users.noreply.github.com>
2024-10-30 10:18:30 +0100

    update dashboard code to use YAML file

--- modules/dashboard/trusted_advisor.tf
+++ modules/dashboard/trusted_advisor.tf
@@ -1,88 +1,11 @@
 locals {
-  trusted_advisor_service_limits = {
-    AutoScaling = [
-      "Auto Scaling groups",
-      "Launch configurations"
-    ]
-    CloudFormation = [
-      "Stacks"
-    ]
-    DynamoDB = [
-      "DynamoDB Read Capacity",
-      "DynamoDB Write Capacity"
-    ]
-    EBS = [
-      "Active snapshots",
-      "Cold HDD (sc1) volume storage (TiB)",
-      "General Purpose SSD (gp2) volume storage (TiB)",
-      "General Purpose SSD (gp3) volume storage",
-      "Magnetic (standard) volume storage (TiB)",
-      "Provisioned IOPS (SSD) storage (TiB)",
-      "Provisioned IOPS SSD (io2) Volume Storage",
-      "Provisioned IOPS",
-      "Throughput Optimized HDD (st1) volume storage (TiB)",
-    ]
-    EC2 = [
-      "Elastic IP addresses (EIPs)",
-      "On-Demand instances"
-    ]
-    ELB = [
-      "Active Application Load Balancers",
-      "Active Network Load Balancers",
-      "Active load balancers",
-    ]
-    Kinesis = [
-      "Shards per region"
-    ]
-    RDS = [
-      "Clusters",
-      "Cluster parameter groups",
-      "DB parameter groups",
-      "DB instances",
-      "Event subscriptions",
-      "RDS DB Manual Snapshots",
-      "Read replicas per master",
-      "Storage quota (GB)",
-      "Subnet groups",
-      "Subnets per subnet group",
-    ]
-    SES = [
-      "Daily sending quota"
-    ]
-    VPC = [
-      "EC2-VPC Elastic IP addresses (EIPs)",
-      "Internet gateways",
-      "VPCs",
-    ]
-  }
-
-  trusted_advisor_metrics_normalized_all = flatten([
-    for region in var.regions : [
-      for service_name, limits in local.trusted_advisor_service_limits : [
-        for resource in limits : {
-          resource     = resource
-          region       = region
-          service_name = service_name
-          id           = lower(replace(format("%s%s", service_name, resource), "/[\\W_]+/", ""))
-          label        = format("%s: %s", service_name, resource)
-        }
-      ] if !contains(var.disabled_services, service_name)
-    ]
-  ])
-
-  trusted_advisor_metrics_normalized_service_region = {
-    for service_name, limits in local.trusted_advisor_service_limits : service_name => {
-      for region in var.regions : region => [for metric in local.trusted_advisor_metrics_normalized_all : metric if metric.region == region && metric.service_name == service_name]
-    }
-  }
-
-  trusted_advisor_dashboard_widgets = flatten([
-    for service_name, region_data in local.trusted_advisor_metrics_normalized_service_region : [
-      for region, metrics in region_data : [
+  regional_metrics = yamldecode(data.local_file.metrics.content)["dashboard_data"]["trusted_advisor_regional"]
+  filtered_regional_metrics = flatten([
+    for service_name, id in local.regional_metrics : [
+      for region in var.regions : [
         {
           type = "metric"
           properties = {
-            stat   = "Sum"
             region = "us-east-1"
             period = 300
             view   = "timeSeries"
@@ -96,15 +19,15 @@ locals {
               }
             }
             metrics = concat([
-              for metric in metrics : flatten([
+              for id, metric_config in id : flatten([
                 [
-                  "AWS/TrustedAdvisor", "ServiceLimitUsage", "ServiceName", metric["service_name"], "ServiceLimit", metric["resource"], "Region", metric["region"],
-                  { id = metric["id"], visible = false }
+                  "AWS/TrustedAdvisor", metric_config["metric_name"], "ServiceName", metric_config["dimensions"]["ServiceName"], "ServiceLimit", metric_config["dimensions"]["ServiceLimit"], "Region", region,
+                  { id = metric_config["dashboard_query_id"], visible = false, stat = metric_config["statistic"] }
                 ]
               ])
               ],
-              [for metric in metrics : [
-                { expression = "${metric.id}*100", label = metric["label"] }
+              [for id, metric_config in id : [
+                { expression = "${metric_config["dashboard_query_id"]}*100", label = format("%s: %s", service_name, metric_config["dimensions"]["ServiceLimit"])}
               ]]
             )
           }
--- modules/dashboard/trusted_advisor_global.tf
+++ modules/dashboard/trusted_advisor_global.tf
@@ -1,73 +1,36 @@
 locals {
-  trusted_advisor_global_service_limits = {
-    IAM = [
-      "Policies",
-      "Groups",
-      "Users",
-      "Instance profiles",
-      "Server certificates",
-      "Roles",
-    ]
-    Route53 = [
-      "Route 53 Max Health Checks",
-      "Route 53 Traffic Policy Instances",
-      "Route 53 Hosted Zones",
-      "Route 53 Reusable Delegation Sets",
-      "Route 53 Traffic Policies",
-    ]
-  }
-
-  trusted_advisor_global_metrics_normalized_all = flatten([
-    for service_name, limits in local.trusted_advisor_global_service_limits : [
-      for resource in limits : {
-        resource     = resource
-        service_name = service_name
-        id           = lower(replace(format("%s%s", service_name, resource), "/[\\W_]+/", ""))
-        label        = format("%s: %s", service_name, resource)
-      }
-    ] if !contains(var.disabled_services, service_name)
-  ])
-
-  trusted_advisor_global_metrics_normalized_service_region = {
-    for service_name, limits in local.trusted_advisor_global_service_limits : service_name => {
-      for region in ["global"] : region => [for metric in local.trusted_advisor_global_metrics_normalized_all : metric if metric.service_name == service_name]
-    }
-  }
-
-  trusted_advisor_global_dashboard_widgets = flatten([
-    for service_name, region_data in local.trusted_advisor_global_metrics_normalized_service_region : [
-      for region, metrics in region_data : [
-        {
-          type = "metric"
-          properties = {
-            stat   = "Sum"
-            region = "us-east-1"
-            period = 300
-            view   = "timeSeries"
-            title  = format("%s: global", service_name)
-            yAxis = {
-              left = {
-                label     = "Quota usage percentage"
-                min       = 0
-                max       = 100
-                showUnits = false
-              }
+  global_metrics = yamldecode(data.local_file.metrics.content)["dashboard_data"]["trusted_advisor_global"]
+  filtered_global_metrics = flatten([
+    for service_name, id in local.global_metrics : [
+      {
+        type = "metric"
+        properties = {
+          region = "us-east-1"
+          period = 300
+          view   = "timeSeries"
+          title  = format("global: %s", service_name)
+          yAxis = {
+            left = {
+              label     = "Quota usage percentage"
+              min       = 0
+              max       = 100
+              showUnits = false
             }
-            metrics = concat([
-              for metric in metrics : flatten([
-                [
-                  "AWS/TrustedAdvisor", "ServiceLimitUsage", "ServiceName", metric["service_name"], "ServiceLimit", metric["resource"], "Region", "-",
-                  { id = metric["id"], visible = false }
-                ]
-              ])
-              ],
-              [for metric in metrics : [
-                { expression = "${metric.id}*100", label = metric["label"] }
-              ]]
-            )
           }
+          metrics = concat([
+            for id, metric_config in id : flatten([
+              [
+                "AWS/TrustedAdvisor", metric_config["metric_name"], "ServiceName", metric_config["dimensions"]["ServiceName"], "ServiceLimit", metric_config["dimensions"]["ServiceLimit"], "Region", "-",
+                { id = metric_config["dashboard_query_id"], visible = false, stat = metric_config["statistic"] }
+              ]
+            ])
+            ],
+            [for id, metric_config in id : [
+              { expression = "${metric_config["dashboard_query_id"]}*100", label = format("%s: %s", service_name, metric_config["dimensions"]["ServiceLimit"])}
+            ]]
+          )
         }
-      ]
+      }
     ]
   ])
 }
